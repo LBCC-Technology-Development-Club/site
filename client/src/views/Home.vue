835c95d56d9ea3b372e852409684c02041bb45e1
John Warila <johnwarila@gmail.com>
2019-09-08 16:36:03 -0700

    Added the v-cards for the layout of home page.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,48 @@
 <template>
-  <h1>Home page</h1>
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          title
+        >
+          Welcome Component
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          title
+        >
+          Current Project Component
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          title
+        >
+          About Team Component
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+
 export default {
   components: {
   }
