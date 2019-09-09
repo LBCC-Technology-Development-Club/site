7230a854e796532c45e6ea12a6ae226081805069
John Warila <johnwarila@gmail.com>
2019-09-09 14:50:01 -0700

    Updated VHomeWelcome.vue to better placeholder text.

--- client/src/components/VHomeWelcome.vue
+++ client/src/components/VHomeWelcome.vue
@@ -1,19 +1,12 @@
 <template>
   <v-container>
     <h1>Welcome to LBCC Technology Development Club!</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
+    <p>We are a club dedicated to developing prototype technologies to enrich our community. Completely student run, with support from LBCC Falculty, we have one driving goal:</p>
+    <h3>Improve the community through advanced technology</h3>
+    <p>Weekly meetings take place to make progress towards this goal, and most of our members work on their own time to improve the designs, and to tend to the adminstrative needs of the club.</p>
+    <hr>
+    <br>
+    <p>If you are interested in joining the club, please send our president Srikar Valluri an email!</p>
   </v-container>
 </template>
 
