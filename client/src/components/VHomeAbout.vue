a6065988908edbc57a30f5f49e55a6eabd1a5b41
John Warila <johnwarila@gmail.com>
2019-09-09 14:50:37 -0700

    Added VHomeAbout.vue, the about component for the home view.

---
+++ client/src/components/VHomeAbout.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>Our Team</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HomeAbout'
+}
+</script>
