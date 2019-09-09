a633d27140d8eb49e4e2898c2bb8b5c954d157b5
John Warila <johnwarila@gmail.com>
2019-09-09 14:51:18 -0700

    Added VCurrentProject.vue, the current project component for the home view.

---
+++ client/src/components/VCurrentProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>Our Current Project</h1>
+    <v-card
+      elevation="4"
+    >
+      <v-img
+        class="white--text"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title
+          class="align-end fill-height"
+        >Prosthetic Leg</v-card-title>
+      </v-img>
+      <v-card-text>A design prototype of the ankle</v-card-text>
+    </v-card>
+    <br>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error at facere neque dolorem quisquam sint in, expedita est dolore sunt odio hic architecto nesciunt soluta quia voluptate autem ipsum delectus.</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'CurrentProject'
+}
+</script>
