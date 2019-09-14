f37b50ebb0f1a8857a353e0da32a167a69d0eb0e
John Warila <johnwarila@gmail.com>
2019-09-14 10:23:20 -0700

    Added a dummy link for a contact page.

--- client/src/components/VHomeWelcome.vue
+++ client/src/components/VHomeWelcome.vue
@@ -1,12 +1,19 @@
 <template>
   <v-container>
-    <h1>Welcome to LBCC Technology Development Club!</h1>
+    <h1>Welcome our Technology Development Club!</h1>
     <p>We are a club dedicated to developing prototype technologies to enrich our community. Completely student run, with support from LBCC Falculty, we have one driving goal:</p>
     <h3>Improve the community through advanced technology</h3>
-    <p>Weekly meetings take place to make progress towards this goal, and most of our members work on their own time to improve the designs, and to tend to the adminstrative needs of the club.</p>
+    <br>
+    <p>Weekly meetings take place to make progress towards this goal, and most of our members work on their own time to improve the designs.</p>
     <hr>
     <br>
     <p>If you are interested in joining the club, please send our president Srikar Valluri an email!</p>
+    <v-btn
+      flat
+      text
+      color="primary"
+      to="/blog"
+    >Contact Us</v-btn>
   </v-container>
 </template>
 
