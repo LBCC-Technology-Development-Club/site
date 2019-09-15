dc6095eace9bd077862fa676fb3eaa73bbdbaa32
John Warila <johnwarila@gmail.com>
2019-09-15 13:22:35 -0700

    Updated About.vue to use new components

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="no-pad grey lighten-5">
+    <v-row>
+      <v-col
+      cols="12"
+      md="1"
+      >
+      </v-col>
+      <v-col
+      cols="12"
+      md="4"
+      >
+        <VAboutText />
+      </v-col>
+      <v-col
+      cols="12"
+      md="6"
+      >
+        <VAboutMembers />
+      </v-col>
+
+      <v-col
+      cols="12"
+      md="1"
+      >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import VAboutText from '@/components/VAboutText.vue'
+import VAboutMembers from '@/components/VAboutMembers.vue'
+
+export default {
+  components: {
+    VAboutText,
+    VAboutMembers
+  }
+}
+</script>
