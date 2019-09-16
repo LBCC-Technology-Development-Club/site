1d5be5d43dcb2f313be21a7c94c92c0c7d742b7c
John Warila <johnwarila@gmail.com>
2019-09-16 11:26:36 -0700

    Added a placeholder for Blog.vue

--- client/src/views/Blog.vue
+++ client/src/views/Blog.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is a blog</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        md="2"
+      >
+      </v-col>
+      <v-col
+        cols="12"
+        md="8"
+      >
+        <VBlogPlaceHolder />
+      </v-col>
+      <v-col
+        cols="12"
+        md="2"
+      >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import VBlogPlaceHolder from '@/components/VBlogPlaceHolder.vue'
+
+export default {
+  components: {
+    VBlogPlaceHolder
+  }
+}
+</script>
