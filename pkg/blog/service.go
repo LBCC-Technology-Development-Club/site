6fc1f80ab3f0f26a5bd7142e28306936b223d6d6
JohnNWarila <johnwarila@gmail.com>
2019-11-17 11:52:33 -0800

    Added RawPost struct to blog

---
+++ pkg/blog/service.go
@@ -0,0 +1,16 @@
+package blog
+
+import (
+	"strconv"
+
+	"github.com/LBCC-Technology-Development-Club/site/pkg/core"
+)
+
+// RawPost describes the structure of raw posts that we get from the client
+type RawPost struct {
+	PostID  int64  `json:"pid"`
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+	Body    string `json:"body"`
+	// Add timestamp here
+}
