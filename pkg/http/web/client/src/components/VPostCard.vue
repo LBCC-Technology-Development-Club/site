a4ba88abfe6e617bca5fe11bb24e77ac0cd56a99
JohnNWarila <johnwarila@gmail.com>
2019-11-17 13:03:42 -0800

    Added VPostCard.vue

---
+++ pkg/http/web/client/src/components/VPostCard.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="postcard">
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>
+      <div>by {{ author }}</div>
+      <br>
+      <div class="text--primary">{{ summary }}</div>
+    </v-card-text>
+  </v-card>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'VPostCard',
+  props: ['title', 'author', 'summary']
+}
+</script>
+
+<style>
+#postcard {
+  padding-bottom: 20px;
+}
+</style>
