c1efec6399c505a0c0e5fdead7b6d81929677572
JohnNWarila <johnwarila@gmail.com>
2019-11-17 13:03:11 -0800

    Added VFeed.vue stub

---
+++ pkg/http/web/client/src/components/VFeed.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="feed">
+    <VPostCard
+    v-for="post in posts"
+    v-bind:key="post.pid"
+    v-bind:title="post.title"
+    v-bind:author="post.author"
+    v-bind:summary="post.summary">
+  </VPostCard>
+</div>
+</template>
+
+<script>
+import VPostCard from '@/components/VPostCard.vue'
+
+export default {
+  name: 'Feed',
+  components: { VPostCard },
+  data () {
+    return {
+      posts: [
+        { pid: 0, title: 'First Post', author: 'John Warila', summary: 'This is the fisrt post' },
+        { pid: 1, title: 'Second Post', author: 'Srikar Valluri', summary: 'This is another post' }
+      ]
+    }
+  }
+}
+</script>
