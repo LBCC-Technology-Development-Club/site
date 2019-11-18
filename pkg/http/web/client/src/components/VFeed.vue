ce969f0b9f71ed157ebb00bace9a55501ff843ac
JohnNWarila <johnwarila@gmail.com>
2019-11-17 19:28:04 -0800

    Updated VFeed.vue to have timestamps, summary, pid

--- pkg/http/web/client/src/components/VFeed.vue
+++ pkg/http/web/client/src/components/VFeed.vue
@@ -1,11 +1,14 @@
 <template>
 <div id="feed">
     <VPostCard
-    v-for="post in posts"
-    v-bind:key="post.pid"
-    v-bind:title="post.title"
-    v-bind:author="post.author"
-    v-bind:summary="post.summary">
+      v-for="post in posts"
+      v-bind:key="post.pid"
+      v-bind:pid="post.pid"
+      v-bind:title="post.title"
+      v-bind:author="post.author"
+      v-bind:timestamp="post.timestamp"
+      v-bind:summary="post.summary"
+    >
   </VPostCard>
 </div>
 </template>
@@ -19,8 +22,8 @@ export default {
   data () {
     return {
       posts: [
-        { pid: 0, title: 'First Post', author: 'John Warila', summary: 'This is the fisrt post' },
-        { pid: 1, title: 'Second Post', author: 'Srikar Valluri', summary: 'This is another post' }
+        { pid: 0, title: 'Our New Blog!', author: 'John Warila', timestamp: 'Nov 17, 2019', summary: 'This article talks about our new blog, and the tech inside.' },
+        { pid: 1, title: 'Weekly Update: Nov 17 2019', author: 'Srikar Valluri', timestamp: 'Nov 17, 2019', summary: 'The weekly update from the week of November 17, 2019. Includes team progress and our meeting minutes.' }
       ]
     }
   }
