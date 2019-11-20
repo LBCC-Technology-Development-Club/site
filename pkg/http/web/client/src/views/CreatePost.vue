2a311a73b62b22fa17fefcdbd4d0df8e81a8e41b
JohnNWarila <johnwarila@gmail.com>
2019-11-19 21:58:28 -0800

    Added CreatePost view

--- pkg/http/web/client/src/views/CreatePost.vue
+++ pkg/http/web/client/src/views/CreatePost.vue
@@ -1,11 +1,32 @@
 <template>
-  <p>createpost</p>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        md="3"
+      >
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <h1>Create New Post</h1>
+        <VPostInput />
+      </v-col>
+      <v-col
+        cols="12"
+        md="3"
+      >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
+import VPostInput from '@/components/VPostInput.vue'
 export default {
   components: {
+    VPostInput
   }
 }
 </script>
