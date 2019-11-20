df94e4aca8768d53992939719cb6f7136a2e2347
JohnNWarila <johnwarila@gmail.com>
2019-11-19 21:58:40 -0800

    Added PostInput component

---
+++ pkg/http/web/client/src/components/VPostInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-text-field label="Title" single-line></v-text-field>
+    <v-text-field label="Summary" single-line></v-text-field>
+    <v-textarea label="Body" auto-grow></v-textarea>
+    <v-btn color="primary">POST</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VPostInput'
+}
+</script>
