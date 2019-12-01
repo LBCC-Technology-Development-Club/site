cb25684d0edb4be4d207b8acbc1ce923817dd1c0
JohnNWarila <johnwarila@gmail.com>
2019-12-01 15:50:31 -0800

    Testing db setup

--- pkg/core/post.go
+++ pkg/core/post.go
@@ -2,8 +2,8 @@ package core
 
 // Post represents a blog post
 type Post struct {
-	UserID    string `json:"user_id"`
-	PostID    string `json:"post_id"`
+	UserID    int    `json:"user_id"`
+	PostID    int    `json:"post_id"`
 	Title     string `json:"title"`
 	Summary   string `json:"summary"`
 	Body      string `json:"body"`
--- pkg/storage/database.go
+++ pkg/storage/database.go
@@ -1,10 +1,12 @@
-package storage
+package main
 
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
 	"os"
+
+	"github.com/LBCC-Technology-Development-Club/site/pkg/core"
+	"github.com/go-sql-driver/mysql"
 )
 
 func connect() *sql.DB {
@@ -13,17 +15,51 @@ func connect() *sql.DB {
 	config.User = os.Getenv("DBUSER")
 	config.Passwd = os.Getenv("DBPASSWORD")
 	config.Net = "tcp"
-	config.Addr = "localhost:3306"
+	config.Addr = os.Getenv("DBADDR")
 	config.DBName = "cs340"
 
 	dsn := config.FormatDSN()
 
+	fmt.Printf("%s\n", dsn)
+
 	db, err := sql.Open("mysql", dsn)
 
 	if err != nil {
-		fmt.Println("Error!")
-		fmt.Println(err.Error())
+		panic(err.Error())
 	}
-
 	return db
+}
+
+// AllPosts gets all posts from db
+func AllPosts() {
+	db := connect()
+
+	selDB, err := db.Query("SELECT * FROM Post")
+	if err != nil {
+		panic(err.Error())
+	}
+	post := core.Post{}
+	res := []core.Post{}
+	for selDB.Next() {
+		var uID, pID int
+		var title, summary, body, timestamp string
+		err = selDB.Scan(&pID, &timestamp, &title, &summary, &body, &uID)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("uID: %d\n", uID)
+		post.UserID = uID
+		fmt.Printf("pID: %d\n", pID)
+		post.PostID = pID
+		fmt.Printf("Title: %s\n", title)
+		post.Title = title
+		post.Summary = summary
+		post.Body = body
+		post.Timestamp = timestamp
+		res = append(res, post)
+	}
+}
+
+func main() {
+	AllPosts()
 }
