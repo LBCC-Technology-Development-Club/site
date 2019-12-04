84d09d95bf325a5f615d5b68e65d21cbb9b1d104
JohnNWarila <johnwarila@gmail.com>
2019-12-03 20:56:44 -0800

    fixed the fix for user struct json

--- pkg/service/user.go
+++ pkg/service/user.go
@@ -2,10 +2,10 @@ package service
 
 // User defines a user's data
 type User struct {
-	UserID  int    `json: "user_id"`
-	Name    string `json: "name"`
-	Email   string `json: "email"`
-	IsAdmin bool   `json: "is_admin"`
-	Hash    string `json: "hash"`
-	Role    string `json: "role"`
+	UserID  int    `json:"user_id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	IsAdmin bool   `json:"is_admin"`
+	Hash    string `json:"hash"`
+	Role    string `json:"role"`
 }
