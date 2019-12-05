9ecf72447aad21db7f89d4e03e67fa3d53fb40ef
JohnNWarila <johnwarila@gmail.com>
2019-12-05 03:43:55 -0800

    add dynamics

--- pkg/http/web/client/src/components/VPostComments.vue
+++ pkg/http/web/client/src/components/VPostComments.vue
@@ -1,10 +1,20 @@
 <template>
 <div id="post-comments">
   <h2>Comments</h2>
-  <VCommentInput />
+  <div id="post-title">
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-text-field label="Comment" single-line v-model="content"></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-btn color="primary" text @click.prevent="getFormValues()">submit</v-btn>
+      </v-col>
+    </v-row>
+  </div>
   <VComment
     v-for="comment in comments"
     v-bind:key="comment.comment_id"
+    v-bind:cID="comment.comment_id"
     v-bind:commenter="comment.author"
     v-bind:timestamp="comment.timestamp"
     v-bind:body="comment.content"
@@ -15,15 +25,44 @@
 
 <script>
 import VComment from '@/components/VComment.vue'
-import VCommentInput from '@/components/VCommentInput.vue'
+import APIClient from '@/apiClient'
 
 export default {
   name: 'VPostComments',
   components: {
-    VCommentInput,
     VComment
   },
-  props: ['comments']
+  data () {
+    return {
+      comments: {},
+      comment: {
+        uID: 0,
+        pID: 0,
+        content: '',
+        timestamp: '',
+      },
+      content: '',
+    }
+  },
+  mounted () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      APIClient.getComments(this.$route.params.id).then(responseJSON => {
+        this.comments = responseJSON
+      })
+    },
+    getFormValues () {
+      this.comment.content = this.content
+      this.comment.uID = 0
+
+      APIClient.postNewComment(this.comment, this.$route.params.id)
+      this.comments = {}
+      this.comment = ''
+      this.fetchData()
+    }
+  }
 }
 </script>
 
--- pkg/http/web/client/src/components/VPostInput.vue
+++ pkg/http/web/client/src/components/VPostInput.vue
@@ -1,14 +1,45 @@
 <template>
-  <div>
-    <v-text-field label="Title" single-line></v-text-field>
-    <v-text-field label="Summary" single-line></v-text-field>
-    <v-textarea label="Body" auto-grow></v-textarea>
-    <v-btn color="primary">POST</v-btn>
-  </div>
+  <v-form>
+    <v-text-field label="Title" single-line v-model="title"></v-text-field>
+    <v-text-field label="Summary" single-line v-model="summary"></v-text-field>
+    <v-textarea label="Body" auto-grow v-model="body"></v-textarea>
+    <v-btn
+      color="primary"
+      @click.prevent="getFormValues()"
+      >POST</v-btn>
+  </v-form>
 </template>
 
 <script>
+import APIClient from '@/apiClient'
+
 export default {
-  name: 'VPostInput'
+  name: 'VPostInput',
+  data () {
+    return {
+      post: {
+        title: '',
+        summary: '',
+        author: '',
+        body: '',
+        timestamp: ''
+      },
+      title: '',
+      summary: '',
+      body: ''
+    }
+  },
+  methods: {
+    getFormValues () {
+      this.post.title = this.title
+      this.post.summary = this.summary
+      this.post.body = this.body
+      this.author = 'Admin Test User'
+
+      APIClient.postNewPost(this.post)
+
+      this.$router.push({ name: 'blog' })
+    }
+  }
 }
 </script>
